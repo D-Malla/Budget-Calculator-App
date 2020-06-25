a96919c4d11baee8e81079cefb2aa9ef5dc84123
Dylan <dmalla@live.com>
2020-06-24 21:04:24 -0700

    Implemented functionality with amount/description input and push to income list

--- src/shared/models/budget-item.model.ts
+++ src/shared/models/budget-item.model.ts
@@ -1,4 +1,14 @@
 export class BudgetItem {
-  description: string;
-  amount: number;
+  
+  constructor(public description: string, public amount: number) { }
+  
+  // The code below is equal to the single line of code above
+
+  // description: string;
+  // amount: number;
+
+  // constructor(description: string, amount: number) {
+  //   this.description = description;
+  //   this.amount = amount;
+  // }
 }

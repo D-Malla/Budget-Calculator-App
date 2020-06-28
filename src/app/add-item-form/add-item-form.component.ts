05f9843c9378841663ba684ab58d7f9fe1959619
Dylan <dmalla@live.com>
2020-06-27 23:45:06 -0700

    Implemented total amount functionality. Budget/Expense Calculator app completed.

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -9,14 +9,24 @@ import { BudgetItem } from './../../shared/models/budget-item.model';
 })
 export class AddItemFormComponent implements OnInit {
 
-  @Input() item: BudgetItem = new BudgetItem('', null);
+  @Input() item: BudgetItem;
   @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+
+  isNewItem: boolean = true;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // if item has a value
+    if (this.item) {
+      // this means that an existing item object was passed into this component
+      // therefore this is not a new item
+      this.isNewItem = false;
+    } else {
+      this.isNewItem = true;
+      this.item = new BudgetItem('', null);
+    }
   }
-
   onSubmit(form: NgForm) {
     this.formSubmit.emit(form.value);
     form.reset();
